1dd0861cd15156fd9c4d07b07f9140c79948d67a
Baezor <64096464+baylorhenshaw@users.noreply.github.com>
2024-05-25 17:20:16 -0500

    🏗️ Change to Hello World

--- go.mod
+++ go.mod
@@ -1,18 +1,3 @@
-module baylor.dev/gotest
+module github.com/baylorhenshaw/google-deploy-test
 
 go 1.22.3
-
-require (
-	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/lipgloss v0.10.0 // indirect
-	github.com/charmbracelet/log v0.4.0 // indirect
-	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.18 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.15.2 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/sys v0.13.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,60 +1,35 @@
+// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"html/template"
+	"log"
 	"net/http"
 	"os"
-
-	"github.com/charmbracelet/log"
 )
 
-type Module struct {
-	Name   string `json:"name"`
-	Author string `json:"author"`
-	Github string `json:"github"`
-}
-
 func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
 
-	modules := registerModules()
-
+	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3001"
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
 
-	log.Info("Server is running on port " + port)
-
-	http.HandleFunc("/{module}", func(w http.ResponseWriter, r *http.Request) {
-		if _, ok := modules[r.PathValue("module")]; ok {
-			tmp := template.Must(template.ParseFiles("templates/module.html"))
-			tmp.Execute(w, modules[r.PathValue("module")])
-		} else {
-			http.NotFound(w, r)
-		}
-	})
-
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-// Register all modules from modules.json
-func registerModules() map[string]Module {
-
-	moduleFile, err := os.ReadFile("modules.json")
-	if err != nil {
-		fmt.Print(err)
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
 	}
-
-	var moduleSlice []Module
-	_ = json.Unmarshal([]byte(moduleFile), &moduleSlice)
-
-	var modules = make(map[string]Module)
-	for i := 0; i < len(moduleSlice); i++ {
-		modules[moduleSlice[i].Name] = moduleSlice[i]
-		log.Info("Registered module: " + moduleSlice[i].Name + " by " + moduleSlice[i].Author + " from " + moduleSlice[i].Github)
-	}
-
-	return modules
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
