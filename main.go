562c30105a1af6dcdd5a69b230010bf482faddfd
Baezor <64096464+baylorhenshaw@users.noreply.github.com>
2024-05-25 23:57:15 -0500

    ✨ Fancy Pages

--- main.go
+++ main.go
@@ -1,35 +1,78 @@
-// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 	"os"
+
+	"github.com/charmbracelet/log"
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+type Config struct {
+	Host    string   `json:"host"`
+	Modules []Module `json:"modules"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
+type Module struct {
+	Name   string `json:"name"`
+	Github string `json:"github"`
+}
+
+func main() {
+
+	modules, config := registerModules()
+
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Info("Defaulting to port " + port)
 	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+
+	log.Info("Server is running on port " + port)
+
+	http.HandleFunc("/{module}", func(w http.ResponseWriter, r *http.Request) {
+		if _, ok := modules[r.PathValue("module")]; ok {
+			tmp := template.Must(template.ParseFiles("templates/module.html"))
+			tmp.Execute(w, modules[r.PathValue("module")])
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			tmp := template.Must(template.ParseFiles("templates/404.html"))
+			tmp.Execute(w, nil)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		type Index struct {
+			Modules []Module
+			Host    string
+		}
+
+		tmp := template.Must(template.ParseFiles("templates/index.html"))
+		tmp.Execute(w, Index{Modules: config.Modules, Host: config.Host})
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// Register all modules from modules.json
+func registerModules() (map[string]Module, Config) {
+
+	configFile, err := os.ReadFile("config.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var config Config
+	_ = json.Unmarshal([]byte(configFile), &config)
+
+	var modules = make(map[string]Module)
+	for i := 0; i < len(config.Modules); i++ {
+		modules[config.Modules[i].Name] = config.Modules[i]
+		log.Info("Registered module " + config.Modules[i].Name + " from " + config.Modules[i].Github)
+	}
+
+	return modules, config
 }
